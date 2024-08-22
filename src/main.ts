7f8a36d094ec66eceac332f338f1ff03b590cd18
Ivan Garcia <ivangris2709@gmail.com>
2024-08-22 13:24:10 +0200

    Añadiendo estilo

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-
-
 // function cambiarResultadoAnterior() {
 //     const resultado = document.getElementById("resultado");
 
@@ -33,45 +31,47 @@
 //     }
 // };
 
-function mostrarResultado (idEvent : string) {
-    const resultado = document.getElementById("resultado");
-    
-    
-    if (resultado !== undefined && resultado !== null && resultado instanceof HTMLHeadingElement)  {
-        if (idEvent == "anterior") {
-            const text = resultado.innerText
-            let numero = Number(text)
-            numero -= 1
-            resultado.innerText = numero.toString().padStart(2, '0')
-            
-        }else if (idEvent == "siguiente") {
-            const text = resultado.innerText
-            let numero = Number(text)
-            numero += 1
-            resultado.innerText = numero.toString().padStart(2, '0')
-        }else if (idEvent == "reset") {
-            const text = resultado.innerText
-            let numero = Number(text)
-            numero = 0
-            resultado.innerText = numero.toString().padStart(2, '0')
-        }
+const mostrarResultado = (idEvent: string) => {
+  const resultado = document.getElementById("resultado");
+
+  if (
+    resultado !== undefined &&
+    resultado !== null &&
+    resultado instanceof HTMLHeadingElement
+  ) {
+    if (idEvent == "anterior") {
+      const text = resultado.innerHTML;
+      let numero = parseInt(text);
+      numero -= 1;
+      resultado.innerHTML = numero.toString().padStart(2, "0");
+    } else if (idEvent == "siguiente") {
+      const text = resultado.innerHTML;
+      let numero = parseInt(text);
+      numero += 1;
+      resultado.innerHTML = numero.toString().padStart(2, "0");
+    } else if (idEvent == "reset") {
+      const text = resultado.innerHTML;
+      let numero = parseInt(text);
+      numero = 0;
+      resultado.innerHTML = numero.toString().padStart(2, "0");
     }
+  }
 };
 
-function cambiarResultado(idUsuario : string) {
-    const resultado = document.getElementById("resultado");
-    const usuario = document.getElementById(idUsuario) as HTMLInputElement;
+const cambiarResultado = (idUsuario: string) => {
+  const resultado = document.getElementById("resultado");
+  const usuario = document.getElementById(idUsuario) as HTMLInputElement;
 
-    if (resultado !== undefined && resultado !== null && 
-        usuario !== undefined && usuario !== null) {
-        let numero = Number(usuario.value)
-        resultado.innerText = numero.toString().padStart(2, '0')
-    }
+  if (
+    resultado !== undefined &&
+    resultado !== null &&
+    usuario !== undefined &&
+    usuario !== null
+  ) {
+    let numero = parseInt(usuario.value);
+    resultado.innerHTML = numero.toString().padStart(2, "0");
+  }
 };
-
-
-
-
 
 const botonAnterior = document.getElementById("anterior");
 const botonSiguiente = document.getElementById("siguiente");
@@ -80,26 +80,25 @@ const aceptar = document.getElementById("aceptar");
 const usuario = document.getElementById("userNumber");
 
 if (botonAnterior !== undefined && botonAnterior !== null) {
-    botonAnterior!.addEventListener("click", () => {
-        mostrarResultado("anterior");
-    });
-};
+  botonAnterior!.addEventListener("click", () => {
+    mostrarResultado("anterior");
+  });
+}
 
 if (botonSiguiente !== undefined && botonSiguiente !== null) {
-    botonSiguiente!.addEventListener("click", () => {
-        mostrarResultado("siguiente");
-    })
-;}
+  botonSiguiente!.addEventListener("click", () => {
+    mostrarResultado("siguiente");
+  });
+}
 
 if (botonReset !== undefined && botonReset !== null) {
-    botonReset!.addEventListener("click", () => {
-        mostrarResultado("reset");
-    })
+  botonReset!.addEventListener("click", () => {
+    mostrarResultado("reset");
+  });
 }
 
 if (aceptar !== undefined && aceptar !== null) {
-    aceptar.addEventListener("click", () => {
-        cambiarResultado("userNumber")
-    })
+  aceptar.addEventListener("click", () => {
+    cambiarResultado("userNumber");
+  });
 }
-
